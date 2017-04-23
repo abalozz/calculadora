9d6adf98feae03c8637a148bcbaec4aef55e9c75
abalozz <abalozz@outlook.com>
2017-04-23 16:48:12 +0200

    Vamos a ponerlo un poco bonito

--- calculadora.js
+++ calculadora.js
@@ -10,15 +10,30 @@ function showTotal(numbers) {
         .reduce((total, number) => total + Number(number), 0);
 }
 
-function calculate() {
-    const numbers = textarea.value.split('\n');
-
+function calculate(numbers) {
     showResultByLine(numbers);
-
     showTotal(numbers);
+}
+
+function onKeyUp(event) {
+    calculate(textarea.value.split(/\r\n|\r|\n/g))
+}
+
+function onKeyPress(event) {
+    const values = textarea.value.split('\n');
+
+    if (event.keyCode === 13) {
+        calculate(values.concat(0));
+    } else if (event.keyCode === 8 && !values[values.length - 1]) {
+        calculate(values.slice(0, -1));
+    } else {
+        calculate(values);
+    }
 }
 
 const textarea = document.querySelector('#input');
 
-textarea.addEventListener('keyup', calculate);
-calculate();
+textarea.addEventListener('keyup', onKeyUp);
+textarea.addEventListener('keypress', onKeyPress);
+
+calculate(textarea.value.split('\n'));
