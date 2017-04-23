37e81a8af45c8e66e615975186b22169d6fb8550
abalozz <abalozz@outlook.com>
2017-04-23 23:04:20 +0200

    Ahora se pueden hacer operaciones en una línea (como 3+2)

--- calculadora.js
+++ calculadora.js
@@ -4,15 +4,45 @@ function showResultByLine(numbers) {
     resultsElement.innerHTML = numbers.map(number => `<div>${Number(number)}</div>`).join('');
 }
 
-function showTotal(numbers) {
-    document.querySelector('#total').innerHTML = numbers
-        .filter(number => !isNaN(number))
-        .reduce((total, number) => total + Number(number), 0);
+function showTotal(total) {
+    document.querySelector('#total').innerHTML = total;
 }
 
-function calculate(numbers) {
-    showResultByLine(numbers);
-    showTotal(numbers);
+function calculateTotal(resultsByLine) {
+    return resultsByLine.filter(number => !isNaN(number))
+        .reduce((total, number) => total + Number(number), 0)
+}
+
+function calculateLinesResult(lines) {
+    return lines.map(line => calculateLineResult(line));
+}
+
+function validateLineCharacters(line) {
+    return line.search(/^[0-9\+\-\*\/\(\) \.xbA-F]*$/g) === 0;
+}
+
+function calculateLineResult(line) {
+    if (!line) {
+        return 0;
+    }
+
+    if (validateLineCharacters(line)) {
+        try {
+            return eval(line);
+        } catch (e) {
+            return NaN;
+        }
+    }
+
+    return NaN;
+}
+
+function calculate(lines) {
+    const resultsByLine = calculateLinesResult(lines);
+    const total = calculateTotal(resultsByLine);
+
+    showResultByLine(resultsByLine);
+    showTotal(total);
 }
 
 function onKeyUp(event) {
@@ -37,7 +67,7 @@ const textarea = document.querySelector('#input');
 if (localStorage.getItem('input')) {
     textarea.value = localStorage.getItem('input');
 } else {
-    textarea.value = ['2', '-3.2', 'No soy un número', '0xA', '0b101'].join('\n');
+    textarea.value = ['2', '-3.2', '2*(3-5)/10', 'No soy un número', '0xA', '0b101'].join('\n');
 }
 
 textarea.addEventListener('keyup', onKeyUp);
